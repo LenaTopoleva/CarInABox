323eb30247757db22d87cef8fd334fd1e2d0b75d
LenaKurasheva <lena.kurasheva@gmail.com>
2021-06-27 15:07:25 +0300

    add customview CarView

--- app/src/main/java/com/lenatopoleva/carinabox/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lenatopoleva/carinabox/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lenatopoleva.carinabox
+package com.lenatopoleva.carinabox.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lenatopoleva.carinabox.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
